e495d8e6b5bc1503abd93e662969e7ab86606d98
Laurynas Keturakis <laurynas.keturakis@gmail.com>
2026-02-25 18:11:46 +0000

    mill 0.1.3

--- Formula/mill.rb
+++ Formula/mill.rb
@@ -1,14 +1,14 @@
 class Mill < Formula
   desc "A runtime for executing TypeScript programs that can spawn agents"
   homepage "https://github.com/laulauland/mill"
-  url "https://github.com/laulauland/mill/archive/refs/tags/v0.1.2.tar.gz"
-  sha256 "5f66dcdd72998e49601dc153b0c66aa54a137836db3e473f9c774a198294e52e"
+  url "https://github.com/laulauland/mill/archive/refs/tags/v0.1.3.tar.gz"
+  sha256 "4434913f105915e2d60e34f913d355d5daa26bca560fdd24ca4db6a8df2d2ea8"
   license "MIT"
 
   bottle do
-    root_url "https://github.com/laulauland/mill/releases/download/v0.1.2"
-    sha256 arm64_sequoia: "cacdb28f05541618a7e0a3d2f55aa723504e2a9566b8ce246d0f8a3f053686e3"
-    sha256 x86_64_linux: "f424f530ade73002e7bdd246fb04e3fe64512a607837b3d24d449fa55c843817"
+    root_url "https://github.com/laulauland/mill/releases/download/v0.1.3"
+    sha256 arm64_sequoia: "a8f45cf8839e91a2172ced9a76262b59648e3547e537c2b720e340777d79d62b"
+    sha256 x86_64_linux: "9ad228887a99c0985e7d3ba7f0d9a6e5364e869dae2472c572cef98c6318dde1"
   end
 
   depends_on "bun" => :build
