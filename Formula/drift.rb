4239a077952a876a8e48f570bfaf32944f396e82
Laurynas Keturakis <laurynas.keturakis@gmail.com>
2026-03-01 23:57:09 +0100

    drift: bump to nightly-3

--- Formula/drift.rb
+++ Formula/drift.rb
@@ -1,26 +1,26 @@
 class Drift < Formula
   desc "Bind specs to code. Lint for drift."
   homepage "https://github.com/laulauland/drift"
-  version "nightly-1"
+  version "nightly-3"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/laulauland/drift/releases/download/nightly-1/drift-aarch64-macos.tar.gz"
-      sha256 "b9a7e2d8708c2170fb88a88bdce4e21e7b3b5367d11af685ff8aacf3b7ae10a2"
+      url "https://github.com/laulauland/drift/releases/download/nightly-3/drift-aarch64-macos.tar.gz"
+      sha256 "fb9f5fcdf90b9177481917b794d287c833eb086fdb8ac3a9110c0706f5ecf563"
     else
-      url "https://github.com/laulauland/drift/releases/download/nightly-1/drift-x86_64-macos.tar.gz"
-      sha256 "e4615efedbe92ae2ec6469f0546750ab8341a8f3cfb0b8b079098901b0ade593"
+      url "https://github.com/laulauland/drift/releases/download/nightly-3/drift-x86_64-macos.tar.gz"
+      sha256 "5cf5799ae7a209431cc83b511e72237a85835811e5b926303a0c4a2da3726fe2"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/laulauland/drift/releases/download/nightly-1/drift-aarch64-linux.tar.gz"
-      sha256 "58380ffa4f349a92643e79fddaa42ca043504aca9109d17935ccd47ea89adec6"
+      url "https://github.com/laulauland/drift/releases/download/nightly-3/drift-aarch64-linux.tar.gz"
+      sha256 "05933981e79f1f5b31c3136e7d1a7ba4b8784054dbaa692be7c428bb83f5b27e"
     else
-      url "https://github.com/laulauland/drift/releases/download/nightly-1/drift-x86_64-linux.tar.gz"
-      sha256 "35318c506cd2cb0b3b580f917d47f37c3e0c325391718850908455525e282257"
+      url "https://github.com/laulauland/drift/releases/download/nightly-3/drift-x86_64-linux.tar.gz"
+      sha256 "65fef368bfdf95761badc1a4c01c52cd806c1be838bc76be6dbd594fd75d8ae9"
     end
   end
 
